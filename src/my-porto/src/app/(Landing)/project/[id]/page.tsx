2e3d23fccead1df36499a4eea1aed5f53c1cd91b
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2025-08-06 22:53:36 +0700

    feat: add detail project

---
+++ src/my-porto/src/app/(Landing)/project/[id]/page.tsx
@@ -0,0 +1,31 @@
+import ProjectDetail from "@/components/Project/Detail";
+
+type Params = Promise<{ id: string }>;
+
+export default async function DetailProjectPage({
+  params,
+}: {
+  params: Params;
+}) {
+  const { id } = await params;
+  return (
+    <main>
+      <div className="relative">
+        <div className="inset-0 -z-10 overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-br from-[#254d70] via-[#254d70] to-orange-50">
+            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#254d70]/20 via-purple-400/10 to-pink-[#254d70] animate-gradient-x"></div>
+
+            <div className="absolute top-20 left-10 w-72 h-72 bg-[#254d70]/30 rounded-full mix-blend-multiply filter blur-xl animate-blob"></div>
+            <div className="absolute top-40 right-10 w-72 h-72 bg-[#254d70]/30 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-2000"></div>
+            <div className="absolute bottom-20 left-20 w-72 h-72 bg-pink-400/30 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-4000"></div>
+            <div className="absolute bottom-40 right-20 w-72 h-72 bg-indigo-400/20 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-6000"></div>
+          </div>
+        </div>
+
+        <div className="px-6 lg:px-[120px] pb-6 lg:pt-[70px] pt-[50px]">
+          <ProjectDetail projectId={id} />
+        </div>
+      </div>
+    </main>
+  );
+}
