02850e8d52bfa5649dba6f8342382ec8020f680a
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2025-10-15 21:51:53 +0700

    feat~server: add blog model

---
+++ src/server/src/api/V2/models/blog.model.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const { database } = require("../../../db/db");
+
+const Blog = database.define(
+  "blog",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    id_user: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "user", // nama tabel di database
+        key: "id",
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true, // Menambahkan createdAt dan updatedAt
+    tableName: "blog", // Nama tabel di database
+  }
+);
+
+module.exports = Blog;
